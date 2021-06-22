49c9beb4b496fd6dfc62d7c3ab46107958035192
joroovb <jorisvanbreugel@mailbox.org>
2021-06-22 14:42:40 +0200

    animation and keymap

--- keyboards/crkbd/keymaps/joroovb/config.h
+++ keyboards/crkbd/keymaps/joroovb/config.h
@@ -23,15 +23,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#define USE_MATRIX_I2C
 
 /* Select hand configuration */
-
 #define MASTER_LEFT
 // #define MASTER_RIGHT
 // #define EE_HANDS
 
 #define USE_SERIAL_PD2
-
-#define TAPPING_FORCE_HOLD
-#define TAPPING_TERM 100
 
 #ifdef RGBLIGHT_ENABLE
     #undef RGBLED_NUM
@@ -54,3 +50,30 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define NO_ACTION_FUNCTION
 
 #define OLED_FONT_H "keyboards/crkbd/lib/glcdfont.c"
+#define OLED_TIMEOUT 120000
+
+// default but used in macros
+#undef TAPPING_TERM
+#define TAPPING_TERM 500
+
+// Prevent normal rollover on alphas from accidentally triggering mods.
+#define IGNORE_MOD_TAP_INTERRUPT
+
+// Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
+#define TAPPING_FORCE_HOLD
+
+// Auto Shift
+#define NO_AUTO_SHIFT_ALPHA
+#define AUTO_SHIFT_TIMEOUT TAPPING_TERM
+#define AUTO_SHIFT_NO_SETUP
+
+// Recommended for heavy chording.
+#define QMK_KEYS_PER_SCAN 4
+
+#define U_NU KC_NO // available but not used
+#define U_NA KC_NO // present but not available for use
+#define U_RDO KC_AGIN
+#define U_PST S(KC_INS)
+#define U_CPY C(KC_INS)
+#define U_CUT S(KC_DEL)
+#define U_UND KC_UNDO
--- keyboards/crkbd/keymaps/joroovb/rules.mk
+++ keyboards/crkbd/keymaps/joroovb/rules.mk
@@ -1,3 +1,5 @@
 OLED_DRIVER_ENABLE  = yes
 WPM_ENABLE = yes
 EXTRAFLAGS += -flto
+EXTRAKEY_ENABLE = yes # Audio control and System control
+AUTO_SHIFT_ENABLE = yes # Auto Shift
