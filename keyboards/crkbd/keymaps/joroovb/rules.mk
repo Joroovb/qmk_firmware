015a967a2506dd9cacba739dba00a5c17794a66f
Joris van Breugel <jorisvanbreugel@mailbox.org>
2021-09-03 22:15:25 +0200

    Please no changes this time.

--- keyboards/crkbd/keymaps/joroovb/rules.mk
+++ keyboards/crkbd/keymaps/joroovb/rules.mk
@@ -1,5 +1,2 @@
-OLED_DRIVER_ENABLE  = yes
-WPM_ENABLE = yes
-EXTRAFLAGS += -flto
 EXTRAKEY_ENABLE = yes # Audio control and System control
-AUTO_SHIFT_ENABLE = yes # Auto Shift
+MOUSEKEY_ENABLE = yes
